05a8effc29209ebab73dc8bab58eee5106055ae3
Robin Penea <robin.penea@gmail.com>
2016-11-24 19:08:36 +0100

    Add about page
    Close #20

---
+++ src/app/foobot-exporter/foobot-exporter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foobot-exporter',
+  templateUrl: './foobot-exporter.component.html',
+  styleUrls: ['./foobot-exporter.component.css']
+})
+export class FoobotExporterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
