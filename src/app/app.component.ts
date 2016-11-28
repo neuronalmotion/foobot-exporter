66f01560fc9a8a1bd6bde989e45f5421724f6d87
Guillaume Lazar <guillaume.lazar@neuronalmotion.com>
2016-11-28 11:43:01 +0100

    Add Google Analytics on pages
    Close #28

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router }   from '@angular/router';
+
+import "rxjs/add/operator/distinctUntilChanged"
+
+// Declare ga function as ambient
+declare var ga: any;
 
 @Component({
   selector: 'app-root',
@@ -6,4 +12,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	constructor(public router: Router) {
+        router.events.distinctUntilChanged((previous: any, current: any) => {
+            if(current instanceof NavigationEnd) {
+                return previous.url === current.url;
+            }
+            return true;
+        }).subscribe((x: any) => {
+            console.log('router.change', x);
+            ga('send', 'pageview', x.url);
+        });
+    }
 }
